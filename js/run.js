553e304e3b8bcbb2b40b5116a08301792b36eb15
David Sheets <sheets@ashimaarts.com>
2011-10-30 23:01:16 -0700

    Fix tabs, back button

--- js/run.js
+++ js/run.js
@@ -1,28 +1,34 @@
-var run_main = false;
+var demo_run = false;
 function run() {
     // the first article in the page is the one we show
     var vdiv = document.getElementsByTagName('article')[0];
     var container = $$("demo-frontis");
     function show_demo() {
-	container.style.display = "none";
-	vdiv.style.display = "";	
+        container.style.display = "none";
+        vdiv.style.display = "";	
     }
     function show_frontis() {
-	container.style.display = "";
-	vdiv.style.display = "none";
+        container.style.display = "";
+        vdiv.style.display = "none";
     }
-    if (!run_main) {
+    if (!demo_run) {
 	window.onpopstate = function (e) {
-	    if (e.state != null && e.state.webgl == 1) show_demo();
-	    else show_frontis();
-	};
-	history.replaceState({webgl:0},"");
-	history.pushState({webgl:1},"");
+            if (e.state != null && e.state.webgl == 1) {
+		show_demo();
+		main();
+	    } else show_frontis();
+        };
+        history.replaceState({webgl:0},"");
+        history.pushState({webgl:1},"");
 
-	show_demo();
+        show_demo();
 
-	main();
+        main();
 
-	run_main = true;
-    } else show_demo();
+        demo_run = true;
+    } else {
+        history.pushState({webgl:1},"");
+        show_demo();
+        main();
+    }
 }
