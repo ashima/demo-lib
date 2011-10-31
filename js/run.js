c696d6d012f547615c6b5a12a2954d70a004523f
David Sheets <sheets@ashimaarts.com>
2011-10-30 21:57:03 -0700

    Don't double load panos on back/view by keeping a bit of state in run(). Fixes GH-1

--- js/run.js
+++ js/run.js
@@ -1,22 +1,28 @@
+var run_main = false;
 function run() {
     // the first article in the page is the one we show
     var vdiv = document.getElementsByTagName('article')[0];
     var container = $$("demo-frontis");
+    function show_demo() {
+	container.style.display = "none";
+	vdiv.style.display = "";	
+    }
+    function show_frontis() {
+	container.style.display = "";
+	vdiv.style.display = "none";
+    }
+    if (!run_main) {
+	window.onpopstate = function (e) {
+	    if (e.state != null && e.state.webgl == 1) show_demo();
+	    else show_frontis();
+	};
+	history.replaceState({webgl:0},"");
+	history.pushState({webgl:1},"");
 
-    window.onpopstate = function (e) {
-	if (e.state != null && e.state.webgl == 1) {
-	    container.style.display = "none";
-	    vdiv.style.display = "";
-	} else {
-	    container.style.display = "";
-	    vdiv.style.display = "none";
-	}
-    };
-    history.replaceState({webgl:0},"");
-    history.pushState({webgl:1},"");
+	show_demo();
 
-    container.style.display = "none";
-    vdiv.style.display = "";
+	main();
 
-    main();
+	run_main = true;
+    } else show_demo();
 }
